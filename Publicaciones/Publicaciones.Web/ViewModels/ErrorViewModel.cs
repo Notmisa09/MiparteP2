d07efcbfe6e9625f7b1e4ba741bd725bb1a6d447
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2023-11-30 13:16:10 -0400

    Last Commit changes

--- Publicaciones/Publicaciones.Web/ViewModels/ErrorViewModel.cs
+++ Publicaciones/Publicaciones.Web/ViewModels/ErrorViewModel.cs
@@ -1,9 +1,12 @@
+using System.Data;
+
 namespace Publicaciones.Web.Models
 {
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
 
+        public bool deleted { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
